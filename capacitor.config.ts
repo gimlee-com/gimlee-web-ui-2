287944aad47fdc0bbd0492b7d1bced7ad8fe7d93
qbns <qbns.spam@gmail.com>
2026-01-11 19:10:41 +0100

    Fix origin and logo in Android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gimlee.app',
   appName: 'Gimlee',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+    hostname: 'gimlee.com'
+  }
 };
 
 export default config;
